c471a844f7129c3bc5512caed7aa1fe0ab1429eb
Robin Raymond <robin@robinraymond.de>
2017-04-19 12:07:17 +0200

    remove ga

--- src/Render.hs
+++ src/Render.hs
@@ -46,13 +46,6 @@ renderPage liqui ladders page =
     H.docTypeHtml $ do
         header
         H.body $ do
-            H.script "\
-\(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){\\n\
-\(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),\\n\
-\m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)\\n\
-\})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');\\n\
-\ga('create', 'UA-97627952-1', 'auto');\\n\
-\ga('send', 'pageview');\\n "
             H.div H.! A.id "layout" $ do
                 menu page
                 H.div H.! A.id "main" $ do
